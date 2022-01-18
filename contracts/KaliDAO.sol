8f6aeec4bd993dc84ece0d4e4a95ff7144446cb8
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-18 15:39:25 -0500

    Update KaliDAO.sol
    add extra security check to avoid null approval in voteBySig

--- contracts/KaliDAO.sol
+++ contracts/KaliDAO.sol
@@ -324,7 +324,7 @@ contract KaliDAO is KaliDAOtoken, Multicall, NFThelper, ReentrancyGuard {
             
         address recoveredAddress = ecrecover(digest, v, r, s);
 
-        if (recoveredAddress != signer) revert InvalidSignature();
+        if (recoveredAddress == address(0) || recoveredAddress != signer) revert InvalidSignature();
         
         _vote(signer, proposal, approve);
     }
