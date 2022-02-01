2d9ca485e7be2bcc00481f4f0c554e266a389db3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-31 23:11:21 -0500

    Update KaliNFT.sol
    🥢nit override

--- contracts/tokens/erc721/public/KaliNFT.sol
+++ contracts/tokens/erc721/public/KaliNFT.sol
@@ -5,16 +5,20 @@ pragma solidity >=0.8.4;
 import "../ERC721.sol";
 
 /// @notice Public NFT minter for Kali DAO.
-abstract contract KaliNFT is ERC721 {
+contract KaliNFT is ERC721 {
     constructor(string memory name_, string memory symbol_)
         ERC721(name_, symbol_)
     {}
+    
+    function tokenURI(uint256) public view override virtual returns (string memory) {
+        return "PLACEHOLDER";
+    }
 
-    function mint(address to, uint256 tokenId) external {
+    function mint(address to, uint256 tokenId) public virtual {
         _mint(to, tokenId);
     }
 
-    function burn(uint256 tokenId) external {
+    function burn(uint256 tokenId) public virtual {
         if (msg.sender != ownerOf[tokenId]) revert NotOwner();
 
         _burn(tokenId);
