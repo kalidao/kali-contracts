249e2be356a93976cc68cc369f17edae11d205b0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 14:48:28 -0500

    Create CallMock.sol
    add mock

---
+++ contracts/mocks/CallMock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.4;
+
+contract CallMock {
+    bool public called;
+
+    fallback() external {
+        called = true;
+    }
+}
