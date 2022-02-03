e0273cf95e4602d44c18526fb8eb6387e487bdd4
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 13:43:42 -0500

    Update and rename IKaliWhitelistManager.sol to IKaliAccessManager.sol
    💄rename

--- contracts/interfaces/IKaliAccessManager.sol
+++ contracts/interfaces/IKaliAccessManager.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.4;
 
-/// @notice Kali DAO whitelist manager interface.
-interface IKaliWhitelistManager {
+/// @notice Kali DAO access manager interface.
+interface IKaliAccessManager {
     function whitelistedAccounts(uint256 listId, address account) external returns (bool);
 }
