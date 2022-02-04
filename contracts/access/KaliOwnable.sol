852087793789c7471b710ff3e9e4879a571caf78
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 21:17:37 -0500

    Update KaliOwnable.sol

--- contracts/access/KaliOwnable.sol
+++ contracts/access/KaliOwnable.sol
@@ -6,26 +6,21 @@ pragma solidity >=0.8.4;
 /// @author Modified from SushiSwap (https://github.com/sushiswap/trident/blob/master/contracts/TridentOwnable.sol)
 abstract contract KaliOwnable {
     event OwnershipTransferred(address indexed from, address indexed to);
-
     event ClaimTransferred(address indexed from, address indexed to);
 
     error NotOwner();
-
     error NotPendingOwner();
 
     address public owner;
-
     address public pendingOwner;
 
     modifier onlyOwner() {
         if (msg.sender != owner) revert NotOwner();
-
         _;
     }
 
     function _init(address owner_) internal {
         owner = owner_;
-
         emit OwnershipTransferred(address(0), owner_);
     }
 
@@ -35,18 +30,15 @@ abstract contract KaliOwnable {
         emit OwnershipTransferred(owner, msg.sender);
 
         owner = msg.sender;
-
         pendingOwner = address(0);
     }
 
     function transferOwner(address to, bool direct) public onlyOwner virtual {
         if (direct) {
             owner = to;
-
             emit OwnershipTransferred(msg.sender, to);
         } else {
             pendingOwner = to;
-
             emit ClaimTransferred(msg.sender, to);
         }
     }
