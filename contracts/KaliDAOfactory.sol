8633f489a6b5fe7c14b121bfba13a24cacb1fd81
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 21:14:37 -0500

    Update KaliDAOfactory.sol

--- contracts/KaliDAOfactory.sol
+++ contracts/KaliDAOfactory.sol
@@ -8,64 +8,61 @@ import './interfaces/IRicardianLLC.sol';
 /// @notice Factory to deploy Kali DAO.
 contract KaliDAOfactory is Multicall {
     event DAOdeployed(
-        KaliDAO indexed kaliDAO, 
-        string name, 
-        string symbol, 
-        string docs, 
-        bool paused, 
-        address[] extensions, 
+        KaliDAO indexed kaliDAO,
+        string name,
+        string symbol,
+        string docs,
+        address[] extensions,
         bytes[] extensionsData,
         address[] voters,
         uint256[] shares,
-        uint32[16] govSettings
+        uint256[] loots,
+        uint32[19] govSettings
     );
-
+ 
     error NullDeploy();
-
-    address payable public immutable kaliMaster;
-
-    IRicardianLLC public immutable ricardianLLC;
-
+ 
+    address payable private immutable kaliMaster;
+    IRicardianLLC private immutable ricardianLLC;
+ 
     constructor(address payable kaliMaster_, IRicardianLLC ricardianLLC_) {
         kaliMaster = kaliMaster_;
-
         ricardianLLC = ricardianLLC_;
     }
-    
+   
     function deployKaliDAO(
         string memory name_,
         string memory symbol_,
         string memory docs_,
-        bool paused_,
         address[] memory extensions_,
         bytes[] memory extensionsData_,
-        address[] calldata voters_,
-        uint256[] calldata shares_,
-        uint32[16] memory govSettings_
+        address[] memory accounts_,
+        uint256[] memory votes_,
+        uint256[] memory loots_,
+        uint32[19] memory govSettings_
     ) public payable virtual returns (KaliDAO kaliDAO) {
         kaliDAO = KaliDAO(_cloneAsMinimalProxy(kaliMaster, name_));
-        
+       
         kaliDAO.init{value: msg.value}(
-            name_, 
-            symbol_, 
+            name_,
+            symbol_,
             docs_,
-            paused_, 
             extensions_,
             extensionsData_,
-            voters_, 
-            shares_,  
+            accounts_,
+            votes_,
+            loots_,
             govSettings_
         );
-
         bytes memory docs = bytes(docs_);
-
+ 
         if (docs.length == 0) {
             ricardianLLC.mintLLC(address(kaliDAO));
         }
-
-        emit DAOdeployed(kaliDAO, name_, symbol_, docs_, paused_, extensions_, extensionsData_, voters_, shares_, govSettings_);
+ 
+        emit DAOdeployed(kaliDAO, name_, symbol_, docs_, extensions_, extensionsData_, accounts_, votes_, loots_, govSettings_);
     }
-
+ 
     /// @dev modified from Aelin (https://github.com/AelinXYZ/aelin/blob/main/contracts/MinimalProxyFactory.sol)
     function _cloneAsMinimalProxy(address payable base, string memory name_) internal virtual returns (address payable clone) {
         bytes memory createData = abi.encodePacked(
@@ -76,9 +73,7 @@ contract KaliDAOfactory is Multicall {
             base,
             bytes15(0x5af43d82803e903d91602b57fd5bf3)
         );
-
         bytes32 salt = keccak256(bytes(name_));
-
         assembly {
             clone := create2(
                 0, // no value
