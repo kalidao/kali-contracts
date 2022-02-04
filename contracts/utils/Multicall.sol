513a7f3d469cff803a1ef9c084ae2a1e4e401edb
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 21:09:35 -0500

    Update Multicall.sol
    style

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -7,19 +7,15 @@ pragma solidity >=0.8.4;
 abstract contract Multicall {
     function multicall(bytes[] calldata data) public virtual returns (bytes[] memory results) {
         results = new bytes[](data.length);
-        
         // cannot realistically overflow on human timescales
         unchecked {
             for (uint256 i = 0; i < data.length; i++) {
                 (bool success, bytes memory result) = address(this).delegatecall(data[i]);
-
                 if (!success) {
                     if (result.length < 68) revert();
-                    
                     assembly {
                         result := add(result, 0x04)
                     }
-                    
                     revert(abi.decode(result, (string)));
                 }
                 results[i] = result;
