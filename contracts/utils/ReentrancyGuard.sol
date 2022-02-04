7bd7bd1ee6f8b4def0e291f2b8150c8bc4cd2304
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-03 21:09:59 -0500

    Update ReentrancyGuard.sol
    style

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -3,25 +3,20 @@
 pragma solidity >=0.8.4;
 
 /// @notice Gas-optimized reentrancy protection.
-/// @author Modified from OpenZeppelin 
+/// @author Modified from OpenZeppelin
 /// (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
 /// License-Identifier: MIT
 abstract contract ReentrancyGuard {
     error Reentrancy();
-
+ 
     uint256 private constant NOT_ENTERED = 1;
-
     uint256 private constant ENTERED = 2;
-
     uint256 private status = NOT_ENTERED;
-
+ 
     modifier nonReentrant() {
         if (status == ENTERED) revert Reentrancy();
-
         status = ENTERED;
-
         _;
-
         status = NOT_ENTERED;
     }
 }
