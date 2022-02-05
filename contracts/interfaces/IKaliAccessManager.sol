a99e032a8b963b9c2c06ebe92f337a85ce5ebbc0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-04 22:16:35 -0500

    Update IKaliAccessManager.sol
    add join function

--- contracts/interfaces/IKaliAccessManager.sol
+++ contracts/interfaces/IKaliAccessManager.sol
@@ -5,4 +5,10 @@ pragma solidity >=0.8.4;
 /// @notice Kali DAO access manager interface.
 interface IKaliAccessManager {
     function listedAccounts(uint256 listId, address account) external returns (bool);
+
+    function joinList(
+        uint256 listId,
+        address account,
+        bytes32[] calldata merkleProof
+    ) external;
 }
