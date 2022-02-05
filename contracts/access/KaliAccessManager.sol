a043fe912e956651c45201fc520750f165a21007
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-04 21:53:49 -0500

    Update KaliAccessManager.sol

--- contracts/access/KaliAccessManager.sol
+++ contracts/access/KaliAccessManager.sol
@@ -5,36 +5,6 @@ pragma solidity >=0.8.4;
 import '../libraries/MerkleProof.sol';
 import '../utils/Multicall.sol';
 
-/// @notice Merkle library adapted from https://github.com/miguelmota/merkletreejs[merkletreejs].
-library MerkleProof {
-    /// @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
-    /// defined by `root`. For this, a `proof` must be provided, containing
-    /// sibling hashes on the branch from the leaf to the root of the tree. Each
-    /// pair of leaves and each pair of pre-images are assumed to be sorted.
-    function verify(
-        bytes32[] memory proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        bytes32 computedHash = leaf;
-
-        for (uint256 i = 0; i < proof.length; i++) {
-            bytes32 proofElement = proof[i];
-
-            if (computedHash <= proofElement) {
-                // Hash(current computed hash + current element of the proof)
-                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
-            } else {
-                // Hash(current element of the proof + current computed hash)
-                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
-            }
-        }
-
-        // check if the computed hash (root) is equal to the provided root
-        return computedHash == root;
-    }
-}
-
 /// @notice Kali DAO access manager.
 /// @author Modified from SushiSwap
 /// (https://github.com/sushiswap/trident/blob/master/contracts/pool/franchised/WhiteListManager.sol)
