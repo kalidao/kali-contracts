9d9d5610e818b9810aaad7364eb91c018efb69b0
audsssy <stamfordh@hotmail.com>
2022-02-13 16:36:15 -0500

    Update KaliNFT.sol
    Update KaliNFT.sol based on latest [NFT contract](https://rinkeby.etherscan.io/address/0xbC208705aF624bE6AE63B27Cf1b94dbD1d7156d0#code)

--- contracts/tokens/erc721/public/KaliNFT.sol
+++ contracts/tokens/erc721/public/KaliNFT.sol
@@ -5,17 +5,23 @@ pragma solidity >=0.8.4;
 import "../ERC721.sol";
 
 /// @notice Public NFT minter for Kali DAO.
-contract KaliNFT is ERC721 {
-    constructor(string memory name_, string memory symbol_)
-        ERC721(name_, symbol_)
-    {}
+contract KaliNFT is ERC721, Multicall {
+    mapping(uint256 => string) private _tokenURI;
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
     
-    function tokenURI(uint256) public view override virtual returns (string memory) {
-        return "PLACEHOLDER";
+    function tokenURI(uint256 tokenId) public view override virtual returns (string memory) {
+        return _tokenURI[tokenId];
     }
 
-    function mint(address to, uint256 tokenId) public virtual {
+    function mint(
+        address to, 
+        uint256 tokenId,
+        string calldata uri
+    ) public virtual {
         _mint(to, tokenId);
+
+        _tokenURI[tokenId] = uri;
     }
 
     function burn(uint256 tokenId) public virtual {
