a2f2773a51b9e59fef8089b3fba5668b312c1122
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-28 17:06:14 -0700

    Update ClubSig.sol
    fix init check

--- contracts/multi-sig/ClubSig.sol
+++ contracts/multi-sig/ClubSig.sol
@@ -71,8 +71,8 @@ contract ClubSig is ERC721initializable, Multicall {
     /// -----------------------------------------------------------------------
 
     struct Signature {
-	uint8 v;
-	bytes32 r;
+	    uint8 v;
+	    bytes32 r;
         bytes32 s;
     }
 
@@ -86,7 +86,7 @@ contract ClubSig is ERC721initializable, Multicall {
         uint256 quorum_,
         bool paused_
     ) public payable virtual {
-        if (nonce == 0) revert Initialized();
+        if (nonce == 1) revert Initialized();
 
         ERC721initializable._init(paused_);
 
@@ -362,68 +362,5 @@ contract ClubSig is ERC721initializable, Multicall {
                 success := 0
             }
         }
-    }
-}
-
-import '../libraries/ClonesWithImmutableArgs.sol';
-
-/// @notice ClubSig Factory.
-contract ClubSigFactory is Multicall {
-    /// -----------------------------------------------------------------------
-    /// Library usage
-    /// -----------------------------------------------------------------------
-    
-    using ClonesWithImmutableArgs for address;
-
-    /// -----------------------------------------------------------------------
-    /// Events
-    /// -----------------------------------------------------------------------
-
-    event SigDeployed(ClubSig indexed clubSig, address[] signers, uint256[] loots, uint256 quorum, bytes32 name, bytes32 symbol, bool paused);
-
-    /// -----------------------------------------------------------------------
-    /// Errors
-    /// -----------------------------------------------------------------------
-
-    error NullDeploy();
-
-    /// -----------------------------------------------------------------------
-    /// Immutable parameters
-    /// -----------------------------------------------------------------------
-
-    ClubSig internal immutable clubMaster;
-
-    /// -----------------------------------------------------------------------
-    /// Constructor
-    /// -----------------------------------------------------------------------
-
-    constructor(ClubSig clubMaster_) {
-        clubMaster = clubMaster_;
-    }
-
-    /// -----------------------------------------------------------------------
-    /// Deployment
-    /// -----------------------------------------------------------------------
-    
-    function deployClubSig(
-        address[] calldata signers_, 
-        uint256[] calldata loots_, 
-        uint256 quorum_,
-        bytes32 name_,
-        bytes32 symbol_,
-        bool paused_
-    ) public payable virtual returns (ClubSig clubSig) {
-        bytes memory data = abi.encodePacked(name_, symbol_);
-
-        clubSig = ClubSig(address(clubMaster).clone(data));
-
-        clubSig.init(
-            signers_, 
-            loots_, 
-            quorum_,
-            paused_
-        );
-
-        emit SigDeployed(clubSig, signers_, loots_, quorum_, name_, symbol_, paused_);
     }
 }
