5cdec32be24f100c190ebe2c8c7ffa581f7f6d9d
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-04-05 02:36:09 +0400

    Update ReentrancyGuard.sol
    solmate

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -1,27 +1,18 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Gas-optimized reentrancy protection.
-/// @author Modified from OpenZeppelin 
-/// (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
-/// License-Identifier: MIT
+/// @notice Gas optimized reentrancy protection for smart contracts
+/// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
+/// License-Identifier: AGPL-3.0-only
 abstract contract ReentrancyGuard {
     error Reentrancy();
-
-    uint256 private constant NOT_ENTERED = 1;
-
-    uint256 private constant ENTERED = 2;
-
-    uint256 private status = NOT_ENTERED;
+    
+    uint256 private locked = 1;
 
     modifier nonReentrant() {
-        if (status == ENTERED) revert Reentrancy();
-
-        status = ENTERED;
-
+        if (locked != 1) revert Reentrancy();
+        locked = 2;
         _;
-
-        status = NOT_ENTERED;
+        locked = 1;
     }
 }
