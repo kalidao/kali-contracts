44f4f79bea3e848e11b7bc3e695f0611890f04cc
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-04-04 14:37:17 +0400

    Update Multicall.sol
    use external visibility and rearrange unchecked block

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -1,28 +1,31 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Helper utility that enables calling multiple local methods in a single call.
+/// @notice Helper utility that enables calling multiple local methods in a single call
 /// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol)
+/// License-Identifier: GPL-2.0-or-later
 abstract contract Multicall {
-    function multicall(bytes[] calldata data) public payable virtual returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) external returns (bytes[] memory results) {
         results = new bytes[](data.length);
         
-        // cannot realistically overflow on human timescales
-        unchecked {
-            for (uint256 i = 0; i < data.length; i++) {
-                (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+        for (uint256 i; i < data.length;) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
 
-                if (!success) {
-                    if (result.length < 68) revert();
+            if (!success) {
+                if (result.length < 68) revert();
                     
-                    assembly {
-                        result := add(result, 0x04)
-                    }
-                    
-                    revert(abi.decode(result, (string)));
+                assembly {
+                    result := add(result, 0x04)
                 }
-                results[i] = result;
+                    
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
+
+            // cannot realistically overflow on human timescales
+            unchecked {
+                ++i;
             }
         }
     }
