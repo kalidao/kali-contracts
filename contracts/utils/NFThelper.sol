1c71e5bb8ba1cbc10ecbdbd8b1e0f4769b1847ff
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-04-04 14:38:33 +0400

    Update NFThelper.sol
    simplification

--- contracts/utils/NFThelper.sol
+++ contracts/utils/NFThelper.sol
@@ -1,16 +1,15 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Helper utility for NFT 'safe' transfers.
-abstract contract NFThelper {
+/// @notice Receiver hook utility for NFT 'safe' transfers
+abstract contract NFTreceiver {
     function onERC721Received(
         address,
         address,
         uint256,
         bytes calldata
-    ) external pure returns (bytes4 sig) {
-        sig = 0x150b7a02; // 'onERC721Received(address,address,uint256,bytes)'
+    ) external pure returns (bytes4) {
+        return 0x150b7a02;
     }
 
     function onERC1155Received(
@@ -19,17 +18,17 @@ abstract contract NFThelper {
         uint256,
         uint256,
         bytes calldata
-    ) external pure returns (bytes4 sig) {
-        sig = 0xf23a6e61; // 'onERC1155Received(address,address,uint256,uint256,bytes)'
+    ) external pure returns (bytes4) {
+        return 0xf23a6e61;
     }
-    
+
     function onERC1155BatchReceived(
         address,
         address,
         uint256[] calldata,
         uint256[] calldata,
         bytes calldata
-    ) external pure returns (bytes4 sig) {
-        sig = 0xbc197c81; // 'onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)'
+    ) external pure returns (bytes4) {
+        return 0xbc197c81;
     }
 }
