cfc22e4f69781c7933ac89fc09809115cf6d5667
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-04-06 19:21:02 +0400

    Update IRicardianLLC.sol
    license and header

--- contracts/interfaces/IRicardianLLC.sol
+++ contracts/interfaces/IRicardianLLC.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Ricardian LLC formation interface.
+/// @notice Ricardian LLC formation interface
 interface IRicardianLLC {
     function mintLLC(address to) external payable;
 }
