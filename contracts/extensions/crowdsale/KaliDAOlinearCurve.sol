27e4ae39391c7ba73bf3e8010b70a6bbb685fcf7
Jordan Teague <ejteague@gmail.com>
2022-05-22 20:07:54 -0400

    Revert "Update KaliDAOlinearCurve.sol"
    This reverts commit 75c44446d23909160e780738ca57bb58f71874c3.

--- contracts/extensions/crowdsale/KaliDAOlinearCurve.sol
+++ contracts/extensions/crowdsale/KaliDAOlinearCurve.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.4;
 
 import '../../libraries/SafeTransferLib.sol';
 import '../../interfaces/IERC20minimal.sol';
-import '../../interfaces/IKaliAccessManager.sol';
+import '../../interfaces/IKaliWhitelistManager.sol';
 import '../../utils/ReentrancyGuard.sol';
 
 /// @notice Crowdsale contract that receives ETH or tokens to mint registered DAO tokens, including merkle whitelisting.
