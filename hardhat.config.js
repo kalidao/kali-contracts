c3b25ca762f083dfe88096a7a512b33607c0ac57
Shiv <shivanshityagi@icloud.com>
2022-06-15 22:57:20 +0530

    🧹 hardhat config
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-//require("hardhat-gas-reporter");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -58,11 +48,11 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.13",
+        version: "0.8.14",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 3333,
+            runs: 11111,
           },
         },
       },
